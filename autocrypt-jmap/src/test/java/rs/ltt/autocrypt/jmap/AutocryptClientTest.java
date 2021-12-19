9bbe0ab0b4ab36758f66e163557d59af9705ecd6
Daniel Gultsch <daniel@gultsch.de>
2021-12-19 11:29:26 +0100

    add tests for AutocryptClientBuilder

--- autocrypt-jmap/src/test/java/rs/ltt/autocrypt/jmap/AutocryptClientTest.java
+++ autocrypt-jmap/src/test/java/rs/ltt/autocrypt/jmap/AutocryptClientTest.java
@@ -31,6 +31,72 @@ public class AutocryptClientTest {
     }
 
     @Test
+    public void nullExecutor() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        AutocryptClient.builder()
+                                .userId("foo@example.com")
+                                .ioExecutorService(null)
+                                .build());
+    }
+
+    @Test
+    public void nullStorage() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> AutocryptClient.builder().userId("foo@example.com").storage(null).build());
+    }
+
+    @Test
+    public void nullDefaultSettings() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () ->
+                        AutocryptClient.builder()
+                                .userId("foo@example.com")
+                                .defaultSettings(null)
+                                .build());
+    }
+
+    @Test
+    public void bbcRecipients() {
+        final Email email =
+                Email.builder()
+                        .bcc(EmailAddress.builder().email("alice@example.com").build())
+                        .build();
+        Assertions.assertThrows(
+                IllegalArgumentException.class, () -> AutocryptClient.recipients(email));
+    }
+
+    @Test
+    public void invalidRecipients() {
+        final Email email = Email.builder().to(EmailAddress.builder().build()).build();
+        IllegalArgumentException iae =
+                Assertions.assertThrows(
+                        IllegalArgumentException.class, () -> AutocryptClient.recipients(email));
+        Assertions.assertEquals("Some recipients do not have email addresses", iae.getMessage());
+    }
+
+    @Test
+    public void noFromDraft() throws ExecutionException, InterruptedException {
+        final AutocryptClient autocryptClient =
+                AutocryptClient.builder().userId("alice@example.com").build();
+        final Email email =
+                Email.builder()
+                        .subject("This is a Test")
+                        .to(EmailAddress.builder().email("bob@example.com").build())
+                        .build();
+
+        final Email processedEmail = autocryptClient.injectAutocryptHeader(email).get();
+
+        final List<String> autocryptHeaders = processedEmail.getAutocrypt();
+        Assertions.assertEquals(1, autocryptHeaders.size());
+        final String headerValue = autocryptHeaders.get(0);
+        assertThat(headerValue, startsWith("addr=alice@example.com;"));
+    }
+
+    @Test
     public void injectIntoEmail() throws ExecutionException, InterruptedException {
         final AutocryptClient autocryptClient =
                 AutocryptClient.builder().userId("alice@example.com").build();
